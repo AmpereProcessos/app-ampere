0042891ad855ee160f6b82deb8f6960c85755cce
Lucas Fernandes <lucasfernandes@hotmail.com>
2022-10-03 18:44:20 -0300

    includes bug fix 4

--- pages/comercial/index.js
+++ pages/comercial/index.js
@@ -203,7 +203,7 @@ function Comercial({ credentials, setCredentials }) {
           </div>
         ))}
       </div>
-      {credentials && credentials.accessibleRoutes.includes("PPS") && (
+      {credentials != {} && credentials.accessibleRoutes.includes("PPS") && (
         <div
           onClick={() => router.push("/comercial/addProjeto")}
           className="fixed bg-[#15599a] cursor-pointer hover:bg-[#fead61] text-white hover:text-[#15599a] p-3 rounded-lg bottom-10 left-150"

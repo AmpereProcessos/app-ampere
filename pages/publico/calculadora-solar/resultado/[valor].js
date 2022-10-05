7fe9c80bb6511fc5ebf433cc017012e937e3d953
AmpereSuporte <amperesuporte@gmail.com>
2022-10-05 14:51:32 -0300

    pixel added in resultado

--- pages/publico/calculadora-solar/resultado/[valor].js
+++ pages/publico/calculadora-solar/resultado/[valor].js
@@ -16,6 +16,7 @@ function EstagioTres({
 }) {
   return (
     <section className="min-h-[100vh] flex items-center justify-center bg-[#15599a]">
+      <Pixel name="FACEBOOK_PIXEL_1" />
       <div className="flex flex-col bg-[#fff] p-4 rounded">
         <div className="flex justify-center">
           <div className="w-[80px] h-[80px]">

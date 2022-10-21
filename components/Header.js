e54c67ddef718788c87dbbe70c4fe7566349a91a
AmpereSuporte <amperesuporte@gmail.com>
2022-10-21 17:59:17 -0300

    scroll added to sidebar

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Link from "next/link";
 import Image from "next/image";
@@ -6,8 +6,18 @@ import { useRouter } from "next/router";
 import WhiteLogo from "../utils/whitelogo.png";
 import { BiLogIn } from "react-icons/bi";
 import { MdAdminPanelSettings } from "react-icons/md";
+import axios from "axios";
 function Header({ toggleSidebar, credentials, logout }) {
   const router = useRouter();
+  const [frase, setFrase] = useState("");
+  {
+    /**   useEffect(() => {
+    axios
+      .get("https://testefunctionsbeto.azurewebsites.net/api/frases-api")
+      .then((res) => setFrase(res.data.texto));
+  }, []);*/
+  }
+
   if (
     router.pathname.includes("pdf") ||
     router.pathname.includes("publico") ||
@@ -17,10 +27,15 @@ function Header({ toggleSidebar, credentials, logout }) {
     return null;
   return (
     <div className="w-full sticky z-[1] top-0 bg-[#fff] grid grid-cols-3 items-center px-12 h-[70px] border-b border-gray-200">
-      <FaBars
-        onClick={toggleSidebar}
-        style={{ fontSize: "23px", color: "#15599a", cursor: "pointer" }}
-      />
+      <div className="flex items-center gap-x-2">
+        <FaBars
+          onClick={toggleSidebar}
+          style={{ fontSize: "23px", color: "#15599a", cursor: "pointer" }}
+        />
+        {/*<p className="hidden lg:block font-bold ml-12 italic font-raleway w-[450px]">
+          "{frase}" - Matheus Oliveira
+  </p>*/}
+      </div>
       <Link href="/">
         <div className="flex cursor-pointer items-center justify-center">
           <Image width={"65px"} height={"65px"} src={WhiteLogo} />
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -14,6 +14,9 @@ import { SiCashapp } from "react-icons/si";
 import { BsFillCalendarEventFill } from "react-icons/bs";
 import { BsFolderPlus } from "react-icons/bs";
 import Link from "next/link";
+const style = {
+  heigth: "calc(100% - 350px)",
+};
 import { useRouter } from "next/router";
 function Sidebar({ credentials }) {
   const router = useRouter();
@@ -25,7 +28,161 @@ function Sidebar({ credentials }) {
   )
     return null;
   return (
-    <div className="flex overflow-y-auto overscroll-y scrollbar-thin scrollbar-thumb-[#155a9a99] scrollbar-track-gray-200 sticky top-[70px] z-[0] flex-col w-[250px] max-h-[900px] border-r border-gray-300">
+    <div
+      style={{ maxHeight: "calc(100vh - 70px)" }}
+      className="flex py-4 px-2 flex-col bg-[#fff] sticky top-[70px] w-[250px] overflow-y-auto overscroll-y scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 border-r border-gray-300"
+    >
+      <div>
+        <h2 className="text-xs text-gray-500">PRINCIPAL</h2>
+        <Link href="/">
+          <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+            <RiDashboardFill style={{ color: "#15599a", fontSize: "20px" }} />
+            <p className="pl-3 text-xs text-gray-600">Dashboard</p>
+          </div>
+        </Link>
+      </div>
+      <div className="mt-6">
+        <h2 className="text-xs text-gray-500">GESTÃO DE PROJETOS</h2>
+        <Link href="/gestaoDeProjetos/emAndamento">
+          <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+            <TbRecharging
+              style={{
+                color: "#15599a",
+                fontSize: "20px",
+              }}
+            />
+            <p className="pl-3 text-xs text-gray-600">Em andamento</p>
+          </div>
+        </Link>
+        <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+          <FaDatabase
+            style={{
+              color: "#15599a",
+              fontSize: "20px",
+            }}
+          />
+          <p className="pl-3 text-xs text-gray-600">Banco de dados</p>
+        </div>
+        <Link href="/calendario">
+          <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+            <BsFillCalendarEventFill
+              style={{ color: "#15599a", fontSize: "20px" }}
+            />
+            <p className="pl-3 text-xs text-gray-600">
+              Cronograma de Obras (em breve)
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className="mt-6">
+        <h2 className="text-xs text-gray-500">SETORES</h2>
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("PPS") && (
+              <Link href="/comercial">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <SiCashapp style={{ color: "#15599a", fontSize: "20px" }} />
+                  <p className="pl-3 text-xs text-gray-600">Comercial</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("Suprimentos") && (
+              <Link href="/suprimentos">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <FaShoppingCart
+                    style={{ color: "#15599a", fontSize: "20px" }}
+                  />
+                  <p className="pl-3 text-xs text-gray-600">Suprimentos</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("Projetos") && (
+              <Link href="/projetos">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <MdEngineering
+                    style={{ color: "#15599a", fontSize: "20px" }}
+                  />
+                  <p className="pl-3 text-xs text-gray-600">Projetos</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("Obras") && (
+              <Link href="/obras">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <FaTools style={{ color: "#15599a", fontSize: "20px" }} />
+                  <p className="pl-3 text-xs text-gray-600">Obras</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("O&M") && (
+              <Link href="/oem">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <FaSolarPanel
+                    style={{ color: "#15599a", fontSize: "20px" }}
+                  />
+                  <p className="pl-3 text-xs text-gray-600">O&M</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("Pós-Venda") && (
+              <Link href="/posvenda">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <BiSupport style={{ color: "#15599a", fontSize: "20px" }} />
+                  <p className="pl-3 text-xs text-gray-600">Pós-Venda</p>
+                </div>
+              </Link>
+            )
+          : false}
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("ADM") && (
+              <Link href="/adm">
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <BsFolderPlus
+                    style={{ color: "#15599a", fontSize: "20px" }}
+                  />
+                  <p className="pl-3 text-xs text-gray-600">ADM</p>
+                </div>
+              </Link>
+            )
+          : false}
+      </div>
+      <div className="mt-6">
+        <h2 className="text-xs text-gray-500">OUTROS</h2>
+        <Link href="/calls">
+          <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+            <FaTasks style={{ color: "#15599a", fontSize: "20px" }} />
+            <p className="pl-3 text-xs text-gray-600">Chamados</p>
+          </div>
+        </Link>
+        {credentials.accessibleRoutes != undefined
+          ? credentials?.accessibleRoutes.includes("ADM") && (
+              <Link href={"/adm/cobrancas"}>
+                <div className="hover:bg-blue-100 py-2 pl-2 cursor-pointer flex items-center mt-2">
+                  <MdOutlinePayments
+                    style={{ color: "#15599a", fontSize: "20px" }}
+                  />
+                  <p className="pl-3 text-xs text-gray-600">Cobranças</p>
+                </div>
+              </Link>
+            )
+          : false}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
+/**
+ *    <div className="flex overflow-y-auto overscroll-y scrollbar-thin scrollbar-thumb-[#155a9a99] scrollbar-track-gray-200 sticky top-[70px] z-[0] flex-col w-[250px] max-h-[900px] border-r border-gray-300">
       <div className="mt-4 flex flex-col h-full pb-2 justify-between">
         <div className="flex flex-col py-4 px-2">
           <div>
@@ -181,7 +338,4 @@ function Sidebar({ credentials }) {
         </div>
       </div>
     </div>
-  );
-}
-
-export default Sidebar;
+ */

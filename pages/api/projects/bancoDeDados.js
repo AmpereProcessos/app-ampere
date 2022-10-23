3368a3637b5209ba1bff7ee22c600784f061793b
Lucas Fernandes <lucasfernandes@hotmail.com>
2022-10-23 17:23:13 -0300

    attemp4

--- pages/api/projects/bancoDeDados.js
+++ pages/api/projects/bancoDeDados.js
@@ -3,8 +3,16 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     const db = await connectToDatabase(process.env.DB_KEY);
     const collection = db.collection("dados");
-    let arr = await collection.find({}).toArray();
-    return res.json(arr);
+    let arr = await collection
+      .aggregate([
+        {
+          $match: {
+            "contrato.status": { $ne: "RECISÃO DE CONTRATO" },
+          },
+        },
+      ])
+      .toArray();
+    res.json(arr);
   }
 }
 export const config = {

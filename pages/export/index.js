55f30aba01fd76d4acffebf78eeedea6996613da
Decctor <lucasfernandes1101@hotmail.com>
2022-11-01 09:15:42 -0300

    export page created

---
+++ pages/export/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Export() {
+  const [projects, setProjects] = useState([]);
+  function getData() {
+    axios.get("/api/export").then((res) => setProjects(res.data));
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="p-6 grow flex flex-col">
+      {projects?.map((project) => (
+        <div>{project.nomeDoContrato}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Export;

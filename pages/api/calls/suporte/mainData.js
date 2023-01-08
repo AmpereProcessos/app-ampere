83fa3de9ecf81da37a2acf7a67f7ade4f50ba371
Lucas Fernandes <lucasfernandes@hotmail.com>
2023-01-08 15:34:17 -0300

    ultAtualizacaoCliente added

--- pages/api/calls/suporte/mainData.js
+++ pages/api/calls/suporte/mainData.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
             tipoChamado: 1,
             statusChamado: 1,
             cidade: 1,
+            ultAtualizacaoCliente: 1,
           },
         },
         {

f86d7bb0b4fce8c0f079669f348c719cd0c4a315
Lucas Fernandes <lucasfernandes@hotmail.com>
2023-01-08 15:29:18 -0300

    update bug fix in callsSuporte

--- pages/api/calls/suporte/updateSuporte.js
+++ pages/api/calls/suporte/updateSuporte.js
@@ -4,17 +4,14 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const db = await connectToDatabase(process.env.DB_KEY);
     const collection = db.collection("suporte");
-    var exists = req.body.fechamento;
+    var changes = { ...req.body };
+    delete changes._id;
     let newDocument = await collection.findOneAndUpdate(
       {
         _id: ObjectId(req.body._id),
       },
       {
-        $set: {
-          fechamento: exists ? new Date().toJSON() : "",
-          statusChamado: req.body.statusChamado,
-          ultAlteracoes: req.body.ultAlteracoes,
-        },
+        $set: { ...changes },
       },
       { returnNewDocument: true }
     );
@@ -22,19 +19,14 @@ export default async function handler(req, res) {
   } else if (req.method === "PUT") {
     const db = await connectToDatabase(process.env.DB_KEY, "chamados");
     const collection = db.collection("suporte");
+    var changes = { ...req.body };
+    delete changes._id;
     let newDocument = await collection.findOneAndUpdate(
       {
         _id: ObjectId(req.body._id),
       },
       {
-        $set: {
-          anotacoes: req.body.anotacoes,
-          responsavel: req.body.responsavel,
-          statusChamado: req.body.statusChamado,
-          ultAlteracoes: req.body.ultAlteracoes,
-          feedbackValor: req.body.feedbackValor,
-          cidade: req.body.cidade,
-        },
+        $set: { ...changes },
       },
       { returnNewDocument: true }
     );

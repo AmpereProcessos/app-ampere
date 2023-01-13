9f9887f10e8b7407fe3e6275609ebcca8ffec914
Lucas Fernandes <lucasfernandes@hotmail.com>
2023-01-12 22:42:59 -0300

    requesting badPerformers locally

--- pages/oem/baixaPerformance.js
+++ pages/oem/baixaPerformance.js
@@ -26,20 +26,39 @@ function BaixaPerformance() {
 
   const [modalItem, setModalItem] = useState({});
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  function getBadPerformers() {
+  async function getBadPerformers() {
     if (token.trim().length > 15) {
       setInProgress(true);
-      axios
-        .get(`/api/o&m/badPerformers?token=${token}`)
-        .then((res) => {
-          setInProgress(false);
-          getToBeAnalized(res.data);
-        })
-        .catch((error) => {
-          setInProgress(false);
-          console.log(error);
-          alert("Erro no servidor.");
+      try {
+        let { data } = await axios.post(
+          "https://api-v2.solarview.com.br/unitList?page=1",
+          {
+            pageSize: 942,
+          },
+          {
+            headers: {
+              "solarview-tokenUniversal": token,
+            },
+          }
+        );
+        var arr = [];
+        data.data.map((user) => {
+          var parsed = JSON.parse(user.consumerUnit30dPerformance);
+          if (parsed != null && typeof parsed == "object") {
+            let dados30d = JSON.parse(user.consumerUnit30dPerformance);
+            let reg = dados30d[4];
+            let nomeUsina = user.consumerUnitName;
+            if (Number(reg) <= 80) {
+              arr.push({ nomeUsina: nomeUsina, performance: Number(reg) });
+            }
+          }
         });
+        setBadPerformers(arr);
+        setInProgress(false);
+      } catch (error) {
+        alert("Erro na requisição.");
+        setInProgress(false);
+      }
     } else {
       setInProgress(false);
       alert("Por favor, preencha um token válido.");

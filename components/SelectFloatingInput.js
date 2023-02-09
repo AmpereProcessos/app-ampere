444499c38e38888494d304f8ae533c8245363eec
AmpereProcessos <lucasfernandes1101@hotmail.com>
2023-02-09 17:41:35 -0300

    attemp to fix iOS bug 3

--- components/SelectFloatingInput.js
+++ components/SelectFloatingInput.js
@@ -21,7 +21,7 @@ function SelectFoatingInput({
         disabled={!editable}
         onChange={(e) => handleChange(e.target.value)}
         onBlur={() => setSelectActive(false)}
-        className="flex py-2.5 text-center px-0 w-full text-sm text-gray-900 bg-transparent font-arial border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        className="flex py-2.5 text-center z-1 px-0 w-full text-sm text-gray-900 bg-[#fff] font-arial border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
       >
         {options &&
           options.map((option, index) => (
@@ -33,7 +33,7 @@ function SelectFoatingInput({
 
       <label
         htmlFor="floating_numberInput"
-        className="peer-focus:font-medium absolute w-full text-sm  text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        className="peer-focus:font-medium absolute w-full text-sm  text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 z-2 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
       >
         {label}
       </label>

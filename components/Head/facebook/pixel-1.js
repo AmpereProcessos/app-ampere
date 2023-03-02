6a6cc252c9fefb3b63b913174968f56f5e04fc48
AmpereProcessos <lucasfernandes1101@hotmail.com>
2023-03-02 11:19:46 -0300

    pixel id ajusted

--- components/Head/facebook/pixel-1.js
+++ components/Head/facebook/pixel-1.js
@@ -3,6 +3,7 @@ import * as fbq from "../../../utils/fpixel";
 function FacebookPixel() {
   return (
     <>
+      {/**Meta Pixel Code */}
       <meta
         name="facebook-domain-verification"
         content="0hufc7839y66urgaxa8ahifp1xpos2"
@@ -17,15 +18,17 @@ function FacebookPixel() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', ${fbq.FB_PIXEL_ID});`,
+          fbq('init', '300806247850748');
+          fbq('track', 'PageView');`,
         }}
       />
       <noscript
         dangerouslySetInnerHTML={{
           __html: `<img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=${fbq.FB_PIXEL_ID}&ev=PageView&noscript=1" />`,
+      src="https://www.facebook.com/tr?id=300806247850748&ev=PageView&noscript=1" />`,
         }}
       />
+      {/**End Meta Pixel Code */}
     </>
   );
 }
--- components/Head/index.js
+++ components/Head/index.js
@@ -1,12 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
 import FacebookPixel from "./facebook/pixel-1";
+import { useRouter } from "next/router";
 
 function AppHead({ name }) {
+  const router = useRouter();
   return (
     <Head>
-      <title>Sistema Ampère Energias</title>
+      <title>
+        {router.pathname.includes("publico")
+          ? "Ampère Energias"
+          : "Sistema Ampère Energias"}
+      </title>
       <FacebookPixel />
     </Head>
   );

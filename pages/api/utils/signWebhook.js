77d87cf96549f1a975d130a900c47699b2ef240a
Decctor <lucasfernandes1101@hotmail.com>
2023-07-03 10:13:32 -0300

    webhook responses fixed

--- pages/api/utils/signWebhook.js
+++ pages/api/utils/signWebhook.js
@@ -2,9 +2,14 @@ import connectToDatabase from "../../../utils/auxiliaresDb";
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
-    const db = await connectToDatabase(process.env.DB_KEY);
-    const collection = db.collection("webhook");
-    console.log(req.body);
-    await collection.insertOne(req.body);
+    try {
+      const db = await connectToDatabase(process.env.DB_KEY);
+      const collection = db.collection("webhook");
+      console.log(req.body);
+      await collection.insertOne(req.body);
+      res.status(201).json({ message: "Documento inserido." });
+    } catch (error) {
+      res.status(501).json({ message: "Erro ao inserir documento." });
+    }
   }
 }

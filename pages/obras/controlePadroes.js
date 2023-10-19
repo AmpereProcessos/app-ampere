4737819c9088cea745b0264cd0d5e69df7dd3a00
Decctor <lucasfernandes1101@hotmail.com>
2023-10-19 16:55:30 -0300

    fix in controlePadroes

--- pages/obras/controlePadroes.js
+++ pages/obras/controlePadroes.js
@@ -1,126 +1,98 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import Select from "react-select";
-import PadraoCard from "../../components/PadraoCard";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
-import LoadingPage from "../../components/utils/LoadingPage";
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import Select from 'react-select'
+import PadraoCard from '../../components/PadraoCard'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import LoadingPage from '../../components/utils/LoadingPage'
 function ControlePadroes() {
-  const router = useRouter();
+  const router = useRouter()
   const { data: session, status } = useSession({
     required: true,
     onUnauthenticated() {
-      router.push("/auth/authHome");
+      router.push('/auth/authHome')
     },
-  });
+  })
 
-  const [projects, setProjects] = useState([]);
-  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [projects, setProjects] = useState([])
+  const [filteredProjects, setFilteredProjects] = useState([])
   const [filters, setFilters] = useState({
-    searchFilter: "",
+    searchFilter: '',
     acStatusFilter: [],
     liberacaoStatus: [],
     segmentoFilter: [],
     parecerFilter: [],
     pendencia: false,
-  });
+  })
   function getProjects() {
-    axios.get("/api/gestaoDeObras/padroes").then((res) => {
-      setFilteredProjects(res.data);
-      setProjects(res.data);
-    });
+    axios.get('/api/gestaoDeObras/padroes').then((res) => {
+      setFilteredProjects(res.data)
+      setProjects(res.data)
+    })
   }
   function filterProjects() {
-    var newArr;
+    var newArr
     if (filters.acStatusFilter.length > 0) {
-      if (!newArr) newArr = projects;
-      newArr = newArr.filter((call) =>
-        filters.acStatusFilter.includes(call.projeto.acStatus)
-      );
+      if (!newArr) newArr = projects
+      newArr = newArr.filter((call) => filters.acStatusFilter.includes(call.projeto.acStatus))
     }
     if (filters.liberacaoStatus.length > 0) {
-      if (!newArr) newArr = projects;
-      newArr = newArr.filter((call) =>
-        filters.liberacaoStatus.includes(call.compra.statusLiberacao)
-      );
+      if (!newArr) newArr = projects
+      newArr = newArr.filter((call) => filters.liberacaoStatus.includes(call.compra.statusLiberacao))
     }
     if (filters.segmentoFilter.length > 0) {
-      if (!newArr) newArr = projects;
-      newArr = newArr.filter((call) =>
-        filters.segmentoFilter.includes(call.segmento)
-      );
+      if (!newArr) newArr = projects
+      newArr = newArr.filter((call) => filters.segmentoFilter.includes(call.segmento))
     }
     if (filters.parecerFilter.length > 0) {
-      if (!newArr) newArr = projects;
-      newArr = projects.filter((project) =>
-        filters.parecerFilter.includes(project.parecer.statusDoParecerDeAcesso)
-      );
+      if (!newArr) newArr = projects
+      newArr = projects.filter((project) => filters.parecerFilter.includes(project.parecer.statusDoParecerDeAcesso))
     }
     if (filters.searchFilter.trim().length > 0) {
-      if (!newArr) newArr = projects;
-      newArr = projects.filter((call) =>
-        call.nomeDoContrato
-          .toUpperCase()
-          .includes(filters.searchFilter.toUpperCase())
-      );
+      if (!newArr) newArr = projects
+      newArr = projects.filter((call) => call.nomeDoContrato.toUpperCase().includes(filters.searchFilter.toUpperCase()))
     }
     if (filters.pendencia) {
-      if (!newArr) newArr = projects;
-      newArr = newArr.filter(
-        (call) =>
-          call.projeto.acStatus != "REALIZADO" &&
-          call.compra.statusLiberacao == "PAGO"
-      );
+      if (!newArr) newArr = projects
+      newArr = newArr.filter((call) => call.projeto.acStatus != 'REALIZADO' && !!call.compra.dataPedido)
     }
-    if (!newArr) setFilteredProjects(projects);
+    if (!newArr) setFilteredProjects(projects)
     else {
-      setFilteredProjects(newArr);
+      setFilteredProjects(newArr)
     }
   }
   function ordenate() {
-    let arr = filteredProjects.sort(
-      (a, b) =>
-        new Date(a.projeto.dataAssDocumentacao).getTime() -
-        new Date(b.projeto.dataAssDocumentacao).getTime()
-    );
-    setFilteredProjects([...arr]);
+    let arr = filteredProjects.sort((a, b) => new Date(a.projeto.dataAssDocumentacao).getTime() - new Date(b.projeto.dataAssDocumentacao).getTime())
+    setFilteredProjects([...arr])
   }
   useEffect(() => {
-    if (session?.user.accessibleRoutes.includes("Obras")) {
-      getProjects();
+    if (session?.user.accessibleRoutes.includes('Obras')) {
+      getProjects()
     } else {
       if (session?.user) {
-        router.push("/");
+        router.push('/')
       }
     }
-  }, [session]);
-  if (status == "loading") return <LoadingPage />;
-  if (status == "authenticated") {
+  }, [session])
+  if (status == 'loading') return <LoadingPage />
+  if (status == 'authenticated') {
     return (
       <div className="p-6 grow bg-[#fff]">
         <div className="flex flex-col w-full items-center border-b border-gray-200 mb-2">
-          <h1 className="text-[#fead61] font-bold text-xl pb-2">
-            CONTROLE DE PADRÕES ({filteredProjects.length})
-          </h1>
+          <h1 className="text-[#fead61] font-bold text-xl pb-2">CONTROLE DE PADRÕES ({filteredProjects.length})</h1>
           <div className="flex flex-wrap w-full items-center gap-x-2 justify-center">
             <input
-              type={"text"}
+              type={'text'}
               placeholder="Digite o nome do contrato"
               value={filters.searchFilter}
-              className={
-                "outline-none p-1.5 rounded border border-gray-200 placeholder:italic"
-              }
-              onChange={(e) =>
-                setFilters({ ...filters, searchFilter: e.target.value })
-              }
+              className={'outline-none p-1.5 rounded border border-gray-200 placeholder:italic'}
+              onChange={(e) => setFilters({ ...filters, searchFilter: e.target.value })}
             />
             <div
-              onClick={() =>
-                setFilters({ ...filters, pendencia: !filters.pendencia })
-              }
+              onClick={() => setFilters({ ...filters, pendencia: !filters.pendencia })}
               className={`${
-                filters.pendencia ? "bg-[#15599a]" : "bg-blue-300"
+                filters.pendencia ? 'bg-[#15599a]' : 'bg-blue-300'
               } rounded h-[36px] flex justify-center cursor-pointer items-center font-bold px-2 text-white`}
             >
               PENDÊNCIAS
@@ -136,24 +108,24 @@ function ControlePadroes() {
               }
               options={[
                 {
-                  value: "COMERCIAL",
-                  label: "COMERCIAL",
+                  value: 'COMERCIAL',
+                  label: 'COMERCIAL',
                 },
                 {
-                  value: "INDUSTRIAL",
-                  label: "INDUSTRIAL",
+                  value: 'INDUSTRIAL',
+                  label: 'INDUSTRIAL',
                 },
                 {
-                  value: "RESIDENCIAL",
-                  label: "RESIDENCIAL",
+                  value: 'RESIDENCIAL',
+                  label: 'RESIDENCIAL',
                 },
                 {
-                  value: "RURAL",
-                  label: "RURAL",
+                  value: 'RURAL',
+                  label: 'RURAL',
                 },
                 {
                   value: undefined,
-                  label: "NÃO DEFINIDO",
+                  label: 'NÃO DEFINIDO',
                 },
               ]}
             />
@@ -168,61 +140,24 @@ function ControlePadroes() {
               }
               options={[
                 {
-                  value: "PENDÊNCIA",
-                  label: "PENDÊNCIA",
+                  value: 'PENDÊNCIA',
+                  label: 'PENDÊNCIA',
                 },
                 {
-                  value: "REALIZADO",
-                  label: "REALIZADO",
+                  value: 'REALIZADO',
+                  label: 'REALIZADO',
                 },
                 {
-                  value: "N/A",
-                  label: "N/A",
+                  value: 'N/A',
+                  label: 'N/A',
                 },
                 {
-                  value: "SOLICITADO COM G.D",
-                  label: "SOLICITADO COM G.D",
+                  value: 'SOLICITADO COM G.D',
+                  label: 'SOLICITADO COM G.D',
                 },
                 {
                   value: undefined,
-                  label: "NÃO DEFINIDO",
-                },
-              ]}
-            />
-            <Select
-              isMulti
-              placeholder="STATUS PAG. KIT"
-              onChange={(e) =>
-                setFilters({
-                  ...filters,
-                  liberacaoStatus: e.map((x) => x.value),
-                })
-              }
-              options={[
-                { value: "PAGO", label: "PAGO" },
-                {
-                  value: "REALIZAR COMPRA",
-                  label: "REALIZAR COMPRA",
-                },
-                {
-                  value: "AGUARDANDO PAGAMENTO DO BANCO",
-                  label: "AGUARDANDO PAGAMENTO DO BANCO",
-                },
-                {
-                  value: "AGUARDANDO CLIENTE PAGAR",
-                  label: "AGUARDANDO CLIENTE PAGAR",
-                },
-                {
-                  value: "AGUARDANDO LIBERAÇÃO DE CRÉDITO",
-                  label: "AGUARDANDO LIBERAÇÃO DE CRÉDITO",
-                },
-                {
-                  value: "AGUARDANDO PARECER DE ACESSO",
-                  label: "AGUARDANDO PARECER DE ACESSO",
-                },
-                {
-                  value: "AGUARDANDO N.F",
-                  label: "AGUARDANDO N.F",
+                  label: 'NÃO DEFINIDO',
                 },
               ]}
             />
@@ -237,68 +172,68 @@ function ControlePadroes() {
               }
               options={[
                 {
-                  label: "AGUARDANDO ASSINATURA",
-                  value: "AGUARDANDO ASSINATURA",
+                  label: 'AGUARDANDO ASSINATURA',
+                  value: 'AGUARDANDO ASSINATURA',
                 },
                 {
-                  label: "AGUARDANDO AUMENTO DE CARGA",
-                  value: "AGUARDANDO AUMENTO DE CARGA",
+                  label: 'AGUARDANDO AUMENTO DE CARGA',
+                  value: 'AGUARDANDO AUMENTO DE CARGA',
                 },
                 {
-                  label: "INICIAR PROJETO",
-                  value: "INICIAR PROJETO",
+                  label: 'INICIAR PROJETO',
+                  value: 'INICIAR PROJETO',
                 },
                 {
-                  label: "SOLICITAR TROCA DE TITULARIDADE",
-                  value: "SOLICITAR TROCA DE TITULARIDADE",
+                  label: 'SOLICITAR TROCA DE TITULARIDADE',
+                  value: 'SOLICITAR TROCA DE TITULARIDADE',
                 },
                 {
-                  label: "AGUARDANDO FATURAMENTO ART",
-                  value: "AGUARDANDO FATURAMENTO ART",
+                  label: 'AGUARDANDO FATURAMENTO ART',
+                  value: 'AGUARDANDO FATURAMENTO ART',
                 },
                 {
-                  label: "AGUARDANDO FORMULÁRIOS",
-                  value: "AGUARDANDO FORMULÁRIOS",
+                  label: 'AGUARDANDO FORMULÁRIOS',
+                  value: 'AGUARDANDO FORMULÁRIOS',
                 },
                 {
-                  label: "AGUARDANDO RESPOSTA DA CONCESSIONARIA",
-                  value: "AGUARDANDO RESPOSTA DA CONCESSIONARIA",
+                  label: 'AGUARDANDO RESPOSTA DA CONCESSIONARIA',
+                  value: 'AGUARDANDO RESPOSTA DA CONCESSIONARIA',
                 },
                 {
-                  label: "AGUARDANDO TROCA DE TITULARIDADE",
-                  value: "AGUARDANDO TROCA DE TITULARIDADE",
+                  label: 'AGUARDANDO TROCA DE TITULARIDADE',
+                  value: 'AGUARDANDO TROCA DE TITULARIDADE',
                 },
                 {
-                  label: "AUMENTO DE CARGA",
-                  value: "AUMENTO DE CARGA",
+                  label: 'AUMENTO DE CARGA',
+                  value: 'AUMENTO DE CARGA',
                 },
                 {
-                  label: "CANCELADO",
-                  value: "CANCELADO",
+                  label: 'CANCELADO',
+                  value: 'CANCELADO',
                 },
                 {
-                  label: "PARECER DE ACESSO APROVADO",
-                  value: "PARECER DE ACESSO APROVADO",
+                  label: 'PARECER DE ACESSO APROVADO',
+                  value: 'PARECER DE ACESSO APROVADO',
                 },
                 {
-                  label: "PENDENCIAS",
-                  value: "PENDENCIAS",
+                  label: 'PENDENCIAS',
+                  value: 'PENDENCIAS',
                 },
                 {
-                  label: "SOLICITAR ACESSO",
-                  value: "SOLICITAR ACESSO",
+                  label: 'SOLICITAR ACESSO',
+                  value: 'SOLICITAR ACESSO',
                 },
                 {
-                  label: "SOLICITAR AUMENTO DE CARGA",
-                  value: "SOLICITAR AUMENTO DE CARGA",
+                  label: 'SOLICITAR AUMENTO DE CARGA',
+                  value: 'SOLICITAR AUMENTO DE CARGA',
                 },
                 {
-                  label: "PARECER DE ACESSO COM OBRAS",
-                  value: "PARECER DE ACESSO COM OBRAS",
+                  label: 'PARECER DE ACESSO COM OBRAS',
+                  value: 'PARECER DE ACESSO COM OBRAS',
                 },
                 {
-                  label: "NÃO DEFINIDO",
-                  value: "NÃO DEFINIDO",
+                  label: 'NÃO DEFINIDO',
+                  value: 'NÃO DEFINIDO',
                 },
               ]}
             />
@@ -319,16 +254,12 @@ function ControlePadroes() {
         </div>
         <div className="flex flex-col gap-y-2">
           {filteredProjects.map((project) => (
-            <PadraoCard
-              credentials={session?.user}
-              project={project}
-              key={project._id}
-            />
+            <PadraoCard credentials={session?.user} project={project} key={project._id} />
           ))}
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default ControlePadroes;
+export default ControlePadroes

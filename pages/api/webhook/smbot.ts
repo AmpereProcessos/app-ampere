040aef77f9caa365363a56dde689ff28eb9ce32b
Decctor <lucasfernandes1101@hotmail.com>
2023-11-16 13:10:39 -0300

    creating customer service on SMBOT

--- pages/api/webhook/smbot.ts
+++ pages/api/webhook/smbot.ts
@@ -2,14 +2,20 @@ import { apiHandler } from '@/utils/api'
 import connectToDatabase from '../../../utils/services/mongodb/projects'
 import { NextApiHandler } from 'next'
 
-type PostResponse = string
+type PostResponse = any
 
 const runSMBOTTest: NextApiHandler<PostResponse> = async (req, res) => {
   const db = await connectToDatabase(process.env.DB_KEY, 'projetos')
   const collection = db.collection('teste')
   const body = req.body
   await collection.insertOne(body)
-  return res.status(201).json('Criado com sucesso !')
+  //f191433e-0d40-40f4-97ed-2abc52477c36
+  return res.status(201).json({
+    type: 'CREATE_CUSTOMER_SERVICE',
+    departmentUUID: 'f191433e-0d40-40f4-97ed-2abc52477c36',
+    text: 'Muito  obrigado ! Um dos nossos atendentes logo entrará em contato.',
+    attachments: [],
+  })
 }
 
 export default apiHandler({ POST: runSMBOTTest })

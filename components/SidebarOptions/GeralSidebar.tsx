dc7c67cbeaf03c3536c5f8cf8596ba70f86d6d96
Decctor <lucasfernandes1101@hotmail.com>
2024-02-29 09:14:22 -0300

    insideSales page inactivated

--- components/SidebarOptions/GeralSidebar.tsx
+++ components/SidebarOptions/GeralSidebar.tsx
@@ -155,14 +155,14 @@ function GeralSidebar({ session, userAccessibleRoutes, userIsManager, userIsCont
               </a>
             </Link>
           ) : null}
-          {checkRoute('InsideSales') ? (
+          {/* {checkRoute('InsideSales') ? (
             <Link href="/insideSales">
               <a className="mt-2 flex cursor-pointer items-center py-2 pl-2 duration-300 ease-in hover:scale-105 hover:bg-blue-100">
                 <MdAddIcCall style={{ color: '#15599a', fontSize: '20px' }} />
                 <p className="pl-3 text-xs text-gray-600">Inside Sales</p>
               </a>
             </Link>
-          ) : null}
+          ) : null} */}
         </div>
         <div className="mt-6">
           <h2 className="text-xs text-gray-500">OUTROS</h2>

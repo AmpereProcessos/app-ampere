b4b3cee7413908bdb835189c6215b1dd47b14a02
Decctor <lucasfernandes1101@hotmail.com>
2024-05-21 17:24:17 -0300

    added execution observations to separation project card

--- components/identificador/almoxarifado/separacao/SeparationProjectCard.tsx
+++ components/identificador/almoxarifado/separacao/SeparationProjectCard.tsx
@@ -45,7 +45,11 @@ function getMissingMaterialAsList(str: string | null | undefined) {
   })
   return formattedSpllited.filter((x) => !!x)
 }
-
+function getObservationsAsList(str: string) {
+  if (!str) return []
+  const spllited = str.split('/')
+  return spllited.filter((x) => !!x)
+}
 function getProjectTypeTag(serviceType: string) {
   return <h1 className={`rounded-full px-2 py-1 text-[0.65rem] font-medium lg:text-xs ${getServiceTypeTagColor(serviceType)}`}>{serviceType}</h1>
 }
@@ -56,6 +60,7 @@ type SeparationProjectCardProps = {
 function SeparationProjectCard({ project }: SeparationProjectCardProps) {
   const purchaseMaterialList = getKitInfoAsList(project.compra?.kitInfo)
   const missingMaterialList = getMissingMaterialAsList(project.material?.materialFaltante)
+  const executionObservations = getObservationsAsList(project.obra.observacoes || '')
   return (
     <div className="flex w-full flex-col rounded-md border border-gray-300 p-4">
       <div className="flex w-full items-center justify-between gap-2">
@@ -99,12 +104,32 @@ function SeparationProjectCard({ project }: SeparationProjectCardProps) {
           </p>
         </div>
       </div>
+      <div className="mt-2 flex h-full max-h-[300px] min-h-[150px] w-full flex-col rounded-lg border border-cyan-500 p-3">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="font-sans text-center  font-bold text-[#353432]">OBSERVAÇÕES DE EXECUÇÃO</h1>
+        </div>
+        <div className="overscroll-y mt-2 flex w-full grow flex-col overflow-y-auto px-2 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300">
+          {executionObservations.length > 0 ? (
+            executionObservations.map((observation, index) => (
+              <div key={index} className="flex w-full items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <BsSuitDiamondFill />
+                  <p className="text-xs tracking-tight text-gray-500">{observation}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="flex grow items-center justify-center">
+              <p className="text-center text-sm italic text-gray-500">Nenhum material adicionado a lista...</p>
+            </div>
+          )}
+        </div>
+      </div>
       <div className="mt-2 flex items-start justify-around gap-2">
-        <div className="flex h-full max-h-[300px] min-h-[150px] w-full flex-col rounded-lg border border-cyan-500 p-3 lg:w-[40%]">
+        <div className="flex h-full max-h-[300px] min-h-[150px] w-full flex-col rounded-lg border border-cyan-500 p-3 lg:w-1/2">
           <div className="flex w-full items-center justify-between">
             <h1 className="font-sans text-center  font-bold text-[#353432]">MATERIAL FALTANTE</h1>
           </div>
-
           <div className="overscroll-y mt-2 flex w-full grow flex-col overflow-y-auto px-2 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300">
             {missingMaterialList.length > 0 ? (
               missingMaterialList.map((equip, index) => (
@@ -125,11 +150,10 @@ function SeparationProjectCard({ project }: SeparationProjectCardProps) {
             )}
           </div>
         </div>
-        <div className="flex h-full max-h-[300px] min-h-[150px] w-full flex-col rounded-lg border border-cyan-500 p-3 lg:w-[40%]">
+        <div className="flex h-full max-h-[300px] min-h-[150px] w-full flex-col rounded-lg border border-cyan-500 p-3 lg:w-1/2">
           <div className="flex w-full items-center justify-between">
             <h1 className="font-sans text-center  font-bold text-[#353432]">MATERIAL DO KIT</h1>
           </div>
-
           <div className="overscroll-y mt-2 flex w-full grow flex-col overflow-y-auto px-2 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300">
             {purchaseMaterialList.length > 0 ? (
               purchaseMaterialList.map((equip, index) => (

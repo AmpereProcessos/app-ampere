b7ab5de340738bc2219a48f0bcdc8335d9bb5ce2
Decctor <lucasfernandes1101@hotmail.com>
2024-07-16 15:20:47 -0300

    temporary fix to modals

--- components/utils/AnimatedModalWrapper.js
+++ components/utils/AnimatedModalWrapper.js
@@ -1,71 +1,78 @@
-import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 const backdrop = {
   visible: { opacity: 1 },
   hidden: { opacity: 0 },
-};
+}
 const modal = {
   hidden: {
-    y: "-45%",
-    x: "-45%",
+    y: '-45%',
+    x: '-45%',
     scale: 0.5,
     opacity: 0.3,
   },
   visible: {
-    y: "-50%",
-    x: "-50%",
+    y: '-50%',
+    x: '-50%',
     scale: 1,
     opacity: 1,
   },
-};
+}
 function AnimatedModalWrapper({ children, modalIsOpen, width, height }) {
   const MODAL_STYLES = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%,-50%)",
-    backgroundColor: "#fff",
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    backgroundColor: '#fff',
     // width: width ? width : "93%",
     // height: height ? height : "98%",
-    borderRadius: "10px",
-    padding: "10px",
+    borderRadius: '10px',
+    padding: '10px',
     zIndex: 1000,
-  };
+  }
   const OVERLAY_STYLES = {
-    position: "fixed",
+    position: 'fixed',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: "rgba(0,0,0,.85)",
+    backgroundColor: 'rgba(0,0,0,.85)',
     zIndex: 1000,
-  };
+  }
   return (
-    <>
-      {modalIsOpen && (
-        <AnimatePresence>
-          <motion.div
-            variants={backdrop}
-            initial="hidden"
-            animate="visible"
-            style={OVERLAY_STYLES}
-          >
-            <motion.div
-              variants={modal}
-              initial="hidden"
-              animate="visible"
-              style={MODAL_STYLES}
-              className={`${width ? `w-[90%] lg:w-[${width}]` : "w-[93%]"} ${
-                height ? `h-[90%] lg:h-[${height}]` : "h-[98%]"
-              }`}
-            >
-              {children}
-            </motion.div>
-          </motion.div>
-        </AnimatePresence>
-      )}
-    </>
-  );
+    <div id="defaultModal" className="fixed bottom-0 left-0 right-0 top-0 z-[100] bg-[rgba(0,0,0,.85)]">
+      <div className="relative left-[50%] top-[50%] z-[100] h-[70%] w-[75%] translate-x-[-50%] translate-y-[-50%] rounded-md bg-[#fff] p-[10px]">
+        {children}
+      </div>
+    </div>
+  )
+  // return (
+  //   <>
+  //     {modalIsOpen && (
+  //       <AnimatePresence>
+  //         <motion.div
+  //           variants={backdrop}
+  //           initial="hidden"
+  //           animate="visible"
+  //           style={OVERLAY_STYLES}
+  //         >
+  //           <motion.div
+  //             variants={modal}
+  //             initial="hidden"
+  //             animate="visible"
+  //             style={MODAL_STYLES}
+  //             className={`${width ? `w-[90%] lg:w-[${width}]` : "w-[93%]"} ${
+  //               height ? `h-[90%] lg:h-[${height}]` : "h-[98%]"
+  //             }`}
+  //           >
+  //             {children}
+  //           </motion.div>
+  //         </motion.div>
+  //       </AnimatePresence>
+  //     )}
+  //   </>
+  // );
 }
 
-export default AnimatedModalWrapper;
+export default AnimatedModalWrapper
--- pages/_app.js
+++ pages/_app.js
@@ -31,18 +31,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         <AppHead />
         <QueryClientProvider client={queryClient}>
           <DndProvider backend={HTML5Backend}>
-            <div className={`flex min-h-[100vh] w-screen max-w-full flex-col bg-[#fff] font-[Inter] xl:min-h-[100vh] ${inter.className}`}>
+            <div className="font-Inter flex min-h-[100vh] w-screen max-w-full  flex-col bg-[#fff] xl:min-h-[100vh]">
               <Header toggleSidebar={() => setSidebarVisible((prev) => !prev)} />
-              <div className="flex min-h-[100%] grow ">
+              <div className="flex min-h-[100%] grow">
                 {sidebarVisible ? <Sidebar sidebarVisible={sidebarVisible} /> : null}
                 <div
                   style={{
                     width: sidebarVisible ? 'calc(100vw - 250px)' : '100%',
                   }}
                   data-expanded={!!sidebarVisible ? 'true' : 'false'}
-                  className={cn('flex grow flex-col', {
-                    'hidden lg:flex': sidebarVisible,
-                  })}
+                  className={`${sidebarVisible ? 'hidden lg:flex lg:flex-col' : 'flex flex-col'} grow`}
                 >
                   <Component sidebarVisible={sidebarVisible} toggleSidebar={() => setSidebarVisible((prev) => !prev)} {...pageProps} />
                   <Toaster />

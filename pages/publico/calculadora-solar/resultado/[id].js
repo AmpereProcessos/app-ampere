4d8f12a94a99f0931f10d290d5e5bc19e0a758cb
AmpereProcessos <ampereprocessos@gmail.com>
2024-09-05 15:08:02 -0300

    fix to calculadora solar result page

--- pages/publico/calculadora-solar/resultado/[id].js
+++ pages/publico/calculadora-solar/resultado/[id].js
@@ -16,28 +16,28 @@ export default function Result(props) {
 
     if (currentHour >= 0 && currentHour < 5) {
       return (
-        <p className="text-xl inline m-0 leading-[1.2] text-gray-600">
+        <p className="m-0 inline text-xl leading-[1.2] text-gray-600">
           Olá <strong className="text-[#15599a]">{clientName}</strong>, tudo bem ?
         </p>
       )
     }
     if (currentHour > 5 && currentHour < 12) {
       return (
-        <p className="text-xl inline m-0 leading-[1.2] text-gray-600">
+        <p className="m-0 inline text-xl leading-[1.2] text-gray-600">
           Bom dia <strong className="text-[#15599a]">{clientName}</strong>, tudo bem ?
         </p>
       )
     }
     if (currentHour > 12 && currentHour < 19) {
       return (
-        <p className="text-xl inline m-0 leading-[1.2] text-gray-600">
+        <p className="m-0 inline text-xl leading-[1.2] text-gray-600">
           Boa tarde <strong className="text-[#15599a]">{clientName}</strong>, tudo bem ?
         </p>
       )
     }
     if (currentHour > 19 && currentHour <= 23) {
       return (
-        <p className="text-xl inline m-0 leading-[1.2] text-gray-600">
+        <p className="m-0 inline text-xl leading-[1.2] text-gray-600">
           Boa noite <strong className="text-[#15599a]">{clientName}</strong>, tudo bem ?
         </p>
       )
@@ -68,19 +68,19 @@ export default function Result(props) {
           style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
       </noscript>
-      <div className={`bg-white flex flex-col grow font-['Raleway']`}>
-        <div className="w-full flex justify-center items-center self-stretch h-[82px] bg-gradient-to-l from-[rgba(13,53,92,1)] to-[rgba(21,89,154,1)]">
-          <div className="flex flex-col justify-center items-center w-[80px]">
+      <div className={`flex grow flex-col bg-white font-['Raleway']`}>
+        <div className="flex h-[82px] w-full items-center justify-center self-stretch bg-gradient-to-l from-[rgba(13,53,92,1)] to-[rgba(21,89,154,1)]">
+          <div className="flex w-[80px] flex-col items-center justify-center">
             {/* <img src={Logo} style={{ width: "100%" }} /> */}
             <Image src={Logo} blurDataURL="image.com" quality={100} height={50} width={50} alt="LOGO" style={{ objectFit: 'cover' }} />
           </div>
         </div>
-        <div className="px-4 py-12 w-[90%] gap-2 flex flex-col justify-center self-center">
-          <div className="flex flex-col gap-4 w-full items-start leading-none relative">
+        <div className="flex w-[90%] flex-col justify-center gap-2 self-center px-4 py-12">
+          <div className="relative flex w-full flex-col items-start gap-4 leading-none">
             <h1 className="text-lg font-black">{props.nome},</h1>
-            <p className="text-lg font-medium">Com os dados que nos passou, criamos uma estimativa de projeto para você.</p>
+            <p className="text-sm font-medium lg:text-lg">Com os dados que nos passou, criamos uma estimativa de projeto para você.</p>
           </div>
-          <p className="text-lg font-medium mt-4">
+          <p className="mt-4 text-sm font-medium lg:text-lg">
             De acordo com seu consumo, sugerimos uma geração mensal de {props.energiaGerada.toFixed(2).replace('.', ',')} kWh, que pode ser alcançada
             com {props.numModulos} placas de 550 Wp.
           </p>
@@ -92,28 +92,28 @@ export default function Result(props) {
               {props.numModulos} placas de 550 Wp
             </p>
           </div> */}
-          <p className="text-lg font-medium">
+          <p className="text-sm font-medium lg:text-lg">
             O investimento aproximado para esse projeto é de {props.valorInvestido ? formatToMoney(props.valorInvestido) : ''}.
           </p>
 
-          <div className="flex items-center justify-center gap-4 mt-6">
-            <div className="flex flex-col p-2 bg-gray-200 min-h-[65px] border border-gray-500 shadow-md w-1/3 rounded-lg">
-              <h1 className="tracking-tight text-[16px] ">Geração mensal aproximada</h1>
+          <div className="mt-6 flex flex-col items-center justify-center gap-4 lg:flex-row">
+            <div className="flex min-h-[65px] w-full flex-col rounded-lg border border-gray-500 bg-gray-200 p-2 shadow-md lg:w-1/3">
+              <h1 className="text-[16px] tracking-tight ">Geração mensal aproximada</h1>
               <div className="flex items-end justify-center gap-1">
-                <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway">
+                <h1 className="flex items-end justify-end font-raleway text-[35px] font-black lg:text-[40px]">
                   {props.energiaGerada.toLocaleString('pt-br', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
                   })}
                 </h1>
-                <p className="flex flex-grow-0 items-end justify-center text-sm text-gray-700 mb-1">KWh</p>
+                <p className="mb-1 flex flex-grow-0 items-end justify-center text-sm text-gray-700">KWh</p>
               </div>
             </div>
-            <div className="flex flex-col p-2 bg-gray-200 min-h-[65px] border border-gray-500 shadow-md w-1/3 rounded-lg">
-              <h1 className="tracking-tight text-[16px] ">Investimento aproximado</h1>
+            <div className="flex min-h-[65px] w-full flex-col rounded-lg border border-gray-500 bg-gray-200 p-2 shadow-md lg:w-1/3">
+              <h1 className="text-[16px] tracking-tight ">Investimento aproximado</h1>
               <div className="flex items-start justify-center gap-1">
-                <p className="flex flex-grow-0 items-end justify-center text-sm text-gray-700 mt-2">R$</p>
-                <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway">
+                <p className="mt-2 flex flex-grow-0 items-end justify-center text-sm text-gray-700">R$</p>
+                <h1 className="flex items-end justify-end font-raleway text-[35px] font-black lg:text-[40px]">
                   {props.valorInvestido.toLocaleString('pt-br', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
@@ -121,10 +121,10 @@ export default function Result(props) {
                 </h1>
               </div>
             </div>
-            <div className="flex flex-col p-2 bg-gray-200 min-h-[65px] border border-gray-500 shadow-md w-1/3 rounded-lg">
-              <h1 className="tracking-tight text-[16px] ">Placas necessárias ({props.potModulos} Wp)</h1>
+            <div className="flex min-h-[65px] w-full flex-col rounded-lg border border-gray-500 bg-gray-200 p-2 shadow-md lg:w-1/3">
+              <h1 className="text-[16px] tracking-tight ">Placas necessárias ({props.potModulos} Wp)</h1>
               <div className="flex items-start justify-center gap-1">
-                <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway">
+                <h1 className="flex items-end justify-end font-raleway text-[35px] font-black lg:text-[40px]">
                   {props.numModulos.toLocaleString('pt-br', {
                     minimumFractionDigits: 0,
                     maximumFractionDigits: 0,
@@ -133,17 +133,17 @@ export default function Result(props) {
               </div>
             </div>
           </div>
-          <h1 className="text-[35px] font-bold text-green-800">Formas de Pagamento</h1>
-          <div className="flex items-center justify-center gap-4">
-            <div className="flex flex-col w-1/3">
+          <h1 className="text-xl font-bold text-green-800 lg:text-[35px]">Formas de Pagamento</h1>
+          <div className="flex flex-col items-center justify-center gap-4 lg:flex-row">
+            <div className="flex w-full flex-col lg:w-1/3">
               <h1 className="font-bold text-green-800">Financiamento bancário</h1>
-              <div className="flex flex-col p-2 bg-[rgba(58,181,74,255)] min-h-[90px] border border-gray-500 shadow-md w-full rounded-lg">
-                <h1 className="tracking-tight text-[16px] text-gray-200">
+              <div className="flex min-h-[90px] w-full flex-col rounded-lg border border-gray-500 bg-[rgba(58,181,74,255)] p-2 shadow-md">
+                <h1 className="text-[16px] tracking-tight text-gray-200">
                   <strong className="text-white">60</strong> parcelas de:
                 </h1>
                 <div className="flex items-start justify-center gap-1">
-                  <p className="flex flex-grow-0 items-end justify-center text-sm text-gray-100 mt-2">R$</p>
-                  <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway text-white">
+                  <p className="mt-2 flex flex-grow-0 items-end justify-center text-sm text-gray-100">R$</p>
+                  <h1 className="flex items-end justify-end font-raleway text-[35px] font-black text-white lg:text-[40px]">
                     {props.parcelaFinanciamento.toLocaleString('pt-br', {
                       minimumFractionDigits: 2,
                       maximumFractionDigits: 2,
@@ -152,15 +152,15 @@ export default function Result(props) {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-1/3">
+            <div className="flex w-full flex-col lg:w-1/3">
               <h1 className="font-bold text-green-800">Cartão de Crédito</h1>
-              <div className="flex flex-col p-2 bg-[rgba(58,181,74,255)] min-h-[90px] border border-gray-500 shadow-md w-full rounded-lg">
-                <h1 className="tracking-tight text-[16px] text-gray-200">
+              <div className="flex min-h-[90px] w-full flex-col rounded-lg border border-gray-500 bg-[rgba(58,181,74,255)] p-2 shadow-md">
+                <h1 className="text-[16px] tracking-tight text-gray-200">
                   <strong className="text-white">12</strong> parcelas de:
                 </h1>
                 <div className="flex items-start justify-center gap-1">
-                  <p className="flex flex-grow-0 items-end justify-center text-sm text-gray-100 mt-2">R$</p>
-                  <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway text-white">
+                  <p className="mt-2 flex flex-grow-0 items-end justify-center text-sm text-gray-100">R$</p>
+                  <h1 className="flex items-end justify-end font-raleway text-[35px] font-black text-white lg:text-[40px]">
                     {props.parcelaCartao.toLocaleString('pt-br', {
                       minimumFractionDigits: 2,
                       maximumFractionDigits: 2,
@@ -169,21 +169,21 @@ export default function Result(props) {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col w-1/3">
+            <div className="flex w-full flex-col lg:w-1/3">
               <h1 className="font-bold">Observações</h1>
-              <div className="flex flex-col items-center justify-center p-2 bg-gray-200 min-h-[90px] border border-gray-300 shadow-md w-full rounded-lg font-semibold text-xs lg:text-sm">
+              <div className="flex min-h-[90px] w-full flex-col items-center justify-center rounded-lg border border-gray-300 bg-gray-200 p-2 text-xs font-semibold shadow-md lg:text-sm">
                 *Todos os valores apresentados nessa simulação são estimativas, podendo, desse modo, variar de acordo com condições externas. Para
                 valores reais, consultar com especialistas.*
               </div>
             </div>
           </div>
-          <h1 className="text-[35px] font-bold text-gray-500">Economia e Retorno</h1>
-          <div className="flex items-center justify-center gap-4">
-            <div className="flex flex-col p-2 py-1 bg-gray-200  min-h-[50px] border border-gray-500 shadow-md rounded-lg w-1/2">
-              <h1 className="tracking-tight text-[16px]">Sua economia ao longo do ano será de:</h1>
+          <h1 className="text-xl font-bold text-gray-500 lg:text-[35px]">Economia e Retorno</h1>
+          <div className="flex flex-col items-center justify-center gap-4 lg:flex-row">
+            <div className="flex min-h-[50px] w-full flex-col rounded-lg border  border-gray-500 bg-gray-200 p-2 py-1 shadow-md lg:w-1/2">
+              <h1 className="text-[16px] tracking-tight">Sua economia ao longo do ano será de:</h1>
               <div className="flex items-start justify-center gap-2">
-                <p className="flex flex-grow-0 items-end justify-center text-sm text-gray-700 mt-2">R$</p>
-                <h1 className="font-black flex items-end justify-end text-[35px] lg:text-[40px] font-raleway">
+                <p className="mt-2 flex flex-grow-0 items-end justify-center text-sm text-gray-700">R$</p>
+                <h1 className="flex items-end justify-end font-raleway text-[35px] font-black lg:text-[40px]">
                   {props.economiaAnual.toLocaleString('pt-br', {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
@@ -192,47 +192,42 @@ export default function Result(props) {
               </div>
             </div>
 
-            <div className="flex flex-col p-2 py-1 bg-gray-200  min-h-[50px] border border-gray-500 shadow-md rounded-lg w-1/2">
-              <h1 className="tracking-tight text-[16px]">Tempo de retorno do investimento</h1>
+            <div className="flex min-h-[50px] w-full flex-col rounded-lg border  border-gray-500 bg-gray-200 p-2 py-1 shadow-md lg:w-1/2">
+              <h1 className="text-[16px] tracking-tight">Tempo de retorno do investimento</h1>
               <div className="flex items-end justify-center gap-2">
-                <p className="text-[35px] lg:text-[40px] font-black font-raleway">{beautifyNumberAsString(props.anosCompletosPayback)}</p>
+                <p className="font-raleway text-[35px] font-black lg:text-[40px]">{beautifyNumberAsString(props.anosCompletosPayback)}</p>
                 <p className="mb-1">anos e</p>
-                <p className="text-[35px] lg:text-[40px] font-black font-raleway">{beautifyNumberAsString(props.mesesCompletosPayback)}</p>
+                <p className="font-raleway text-[35px] font-black lg:text-[40px]">{beautifyNumberAsString(props.mesesCompletosPayback)}</p>
                 <p className="mb-1">meses</p>
               </div>
             </div>
           </div>
-          <h1 className="text-lg font-medium">Mas não é só isso...</h1>
-          <h1 className="text-lg font-medium">
+          <h1 className="text-sm font-medium lg:text-lg">Mas não é só isso...</h1>
+          <h1 className="text-sm font-medium lg:text-lg">
             Ter um Sistema Ampère em sua casa não é apenas uma escolha econômica e ambientalmente consciente, mas também um símbolo de status na
             sociedade.
           </h1>
-          <h1 className="text-lg font-medium">
+          <h1 className="text-sm font-medium lg:text-lg">
             Ao optar por nossa empresa, você está escolhendo a qualidade e a segurança que só uma marca forte, que oferece 5 anos de garantia da
             instalação e possui mais de 1.500 clientes no portfólio como a nossa, pode oferecer.
           </h1>
-          <h1 className="text-lg font-medium">
+          <h1 className="text-sm font-medium lg:text-lg">
             Aqui seu projeto passa por mais de 10 engenheiros antes de chegar até você, desde o atendimento até o suporte técnico após a instalação!
           </h1>
-          <div className="flex w-full justify center gap-4">
-            <h1 className="text-lg font-medium">
+          <div className="justify center flex w-full gap-4">
+            <h1 className="text-sm font-medium lg:text-lg">
               <strong>{props.nome}</strong>, aguardamos seu contato no telefone <strong className="text-[#fead41]">(34) 3700-7001</strong> !
             </h1>
           </div>
         </div>
-        <div className="flex flex-col justify-center items-center gap-2 px-10 w-full h-[175px] bg-gradient-to-l from-[rgba(13,53,92,1)] to-[rgba(21,89,154,1)]">
-          <div className="flex items-center justify-center gap-3">
-            <div className="flex flex-col justify-center items-center w-[80px]">
-              {/* <img src={Logo} style={{ width: "100%" }} /> */}
-              <Image src={Logo} placeholder="blur" quality={100} height={40} width={40} alt="LOGO" style={{ objectFit: 'cover' }} />
+        <div className="flex h-[175px] w-full flex-col items-center justify-center gap-2 bg-gradient-to-l from-[rgba(13,53,92,1)] to-[rgba(21,89,154,1)] px-10">
+          <div className="flex flex-col items-center justify-center gap-3 lg:flex-row">
+            <div className="relative h-[80px] w-[80px]">
+              <Image src={Logo} quality={100} fill={true} alt="LOGO" />
             </div>
-            <div className="flex flex-col justify-center items-center">
-              <div className="w-full leading-none relative">
-                <p className="font-normal text-white inline m-0 text-[15px] leading-[1.2]">A energia que move o mundo</p>
-                <p className="text-xs font-normal text-white inline m-0 leading-[1.2]"> </p>
-                <p className="font-black inline m-0 text-[15px] leading-[1.2] text-[rgba(254,173,65,1)]">vem de você</p>
-                <p className="text-xs font-normal text-white inline m-0 leading-[1.2]">!</p>
-              </div>
+            <div className="flex flex-col items-center gap-1 lg:flex-row">
+              <p className="text-center font-medium text-white">A energia que move o mundo</p>
+              <p className="font-black text-[#fead41]">vem de você !</p>
             </div>
           </div>
           {/* <div className="w-full flex items-center justify-center gap-3">
@@ -278,10 +273,10 @@ export async function getServerSideProps({ query }) {
   var investiment = 0
   var effectivePeakPower = 0
 
-  moduleQty = Math.ceil((energyConsumption * 1000) / (120 * 565))
-  modulePower = 565
+  moduleQty = Math.ceil((energyConsumption * 1000) / (120 * 555))
+  modulePower = 555
   effectivePeakPower = (moduleQty * modulePower) / 1000
-  investiment = effectivePeakPower * 3600
+  investiment = effectivePeakPower * 3000
 
   let economiaAnual = Number(Number((effectivePeakPower * 120 * 12 * 0.85).toFixed(2)))
 

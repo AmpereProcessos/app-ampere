1c66cc48ef2cdcdf9da19b3684b9e8f0555cb96c
decctor <lucasfernandes1101@hotmail.com>
2024-09-26 10:58:43 -0300

    add editing for payment options in credit card options

--- components/identificador/opcoes-cartao-credito/EditCreditCardOption.tsx
+++ components/identificador/opcoes-cartao-credito/EditCreditCardOption.tsx
@@ -38,7 +38,7 @@ function EditCreditCardOption({ optionId, closeModal }: EditCreditCardOptionProp
   }, [option])
   return (
     <div id="edit-credit-card-option" className="fixed bottom-0 left-0 right-0 top-0 z-[100] bg-[rgba(0,0,0,.85)]">
-      <div className="fixed left-[50%] top-[50%] z-[100] h-[80%] w-[90%] translate-x-[-50%] translate-y-[-50%] rounded-md bg-[#fff] p-[10px] lg:h-[60%] lg:w-[50%]">
+      <div className="fixed left-[50%] top-[50%] z-[100] h-[80%] w-[90%] translate-x-[-50%] translate-y-[-50%] rounded-md bg-[#fff] p-[10px]  lg:w-[50%]">
         <div className="flex h-full flex-col">
           <div className="flex flex-col items-center justify-between border-b border-gray-200 px-2 pb-2 text-lg lg:flex-row">
             <h3 className="text-xl font-bold text-[#353432] dark:text-white ">EDITAR OPÇÃO</h3>
--- components/identificador/opcoes-cartao-credito/PaymentOptionsMenu.tsx
+++ components/identificador/opcoes-cartao-credito/PaymentOptionsMenu.tsx
@@ -5,6 +5,7 @@ import { formatDecimalPlaces, GeneralVisibleHiddenExitMotionVariants } from '@/u
 import { isEmpty } from '@/utils/methods/shared'
 import { TCreditCardOption } from '@/utils/schemas/credit-card-options'
 import { AnimatePresence, motion } from 'framer-motion'
+import { Pencil } from 'lucide-react'
 import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 import { MdDelete, MdPayment } from 'react-icons/md'
@@ -22,8 +23,11 @@ function PaymentOptionsMenu({ infoHolder, setInfoHolder }: PaymentOptionsMenuPro
     options.push(option)
     setInfoHolder((prev) => ({ ...prev, opcoes: options }))
   }
-  function removePaymentOptions(index: number) {
+  function removePaymentOption(index: number) {
     setInfoHolder((prev) => ({ ...prev, opcoes: prev.opcoes.filter((o, i) => i != index) }))
+  }
+  function updatePaymentOption({ index, data }: { index: number; data: TCreditCardOption['opcoes'][number] }) {
+    setInfoHolder((prev) => ({ ...prev, opcoes: prev.opcoes.map((op, i) => (i == index ? data : op)) }))
   }
   return (
     <div className="flex w-full flex-col gap-2">
@@ -45,32 +49,12 @@ function PaymentOptionsMenu({ infoHolder, setInfoHolder }: PaymentOptionsMenuPro
       <AnimatePresence>{newPaymentOptionMenuIsOpen ? <NewPaymentOptionMenu addPaymentOption={addPaymentOption} /> : null}</AnimatePresence>
       <h1 className="text-sm font-medium tracking-tight text-gray-500">LISTA DE OPÇÕES</h1>
       {infoHolder.opcoes.map((option, index) => (
-        <div key={`${option.descricao}-${index}`} className="flex w-full flex-col rounded border border-gray-500 p-3">
-          <div className="flex w-full items-center justify-between gap-2">
-            <div className="flex items-center gap-1">
-              <div className="flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black p-1">
-                <MdPayment />
-              </div>
-              <p className="text-[0.6rem] font-medium leading-none tracking-tight lg:text-xs">{option.descricao}</p>
-            </div>
-            <button
-              onClick={() => removePaymentOptions(index)}
-              className="flex items-center gap-1 rounded-lg bg-red-600 px-2 py-1 text-[0.6rem] text-white hover:bg-red-500"
-            >
-              <MdDelete width={10} height={10} />
-              <p>REMOVER</p>
-            </button>
-          </div>
-          <h1 className="text-[0.6rem] text-gray-500">TAXAS POR PARCELA</h1>
-          <div className="flex w-full flex-wrap items-center gap-2">
-            {Object.entries(option.parcelas).map(([key, value]) => (
-              <div className="flex items-center gap-1 rounded-lg bg-gray-500 px-2 py-0.5 text-[0.6rem] text-white">
-                <p>{key}x</p>
-                <h1 className="font-black">{formatDecimalPlaces(value)}%</h1>
-              </div>
-            ))}
-          </div>
-        </div>
+        <PaymentOptionCard
+          key={`${option.descricao}-${index}`}
+          option={option}
+          removePaymentOption={() => removePaymentOption(index)}
+          updatePaymentOption={(data) => updatePaymentOption({ index, data })}
+        />
       ))}
     </div>
   )
@@ -95,7 +79,7 @@ function NewPaymentOptionMenu({ addPaymentOption }: NewPaymentOptionMenuProps) {
       exit="exit"
       className="flex w-[90%] flex-col gap-1 self-center overflow-hidden rounded border border-green-500"
     >
-      <h1 className="w-full bg-green-500 p-1 text-center font-bold text-white">NOVA OPÇÃO PAGAMENTO</h1>
+      <h1 className="w-full bg-green-500 p-1 text-center text-xs font-bold text-white lg:text-base">NOVA OPÇÃO PAGAMENTO</h1>
       <div className="flex w-full flex-col gap-2 p-3">
         <TextInput
           value={paymentOption.descricao}
@@ -104,7 +88,7 @@ function NewPaymentOptionMenu({ addPaymentOption }: NewPaymentOptionMenuProps) {
           handleChange={(value) => setPaymentOption((prev) => ({ ...prev, descricao: value }))}
           width="100%"
         />
-        <div className="grid w-full grid-cols-2 items-center gap-2">
+        <div className="grid w-full grid-cols-1 items-center gap-2 lg:grid-cols-2">
           {Object.entries(paymentOption.parcelas).map(([key, value]) => (
             <div className={`flex  w-full flex-col gap-1`}>
               <label htmlFor={`parcela-${key}`} className={cn('text-xs tracking-tight')}>
@@ -122,19 +106,128 @@ function NewPaymentOptionMenu({ addPaymentOption }: NewPaymentOptionMenuProps) {
                   }))
                 }}
                 id={`parcela-${key}`}
-                type="text"
+                type="number"
                 placeholder={`Preencha aqui a taxa para ${key} parcela(s)`}
-                className="h-[47px] w-full rounded-md border border-gray-200 p-3 text-sm outline-none placeholder:italic"
+                className="h-[30px] w-full rounded-md border border-gray-200 p-3 text-sm outline-none placeholder:italic"
               />
             </div>
           ))}
         </div>
         <div className="flex w-full items-center justify-end">
-          <Button onClick={() => addPaymentOption(paymentOption)} size={'sm'}>
+          <Button onClick={() => addPaymentOption(paymentOption)} size={'xs'}>
             ADICIONAR OPÇÃO
           </Button>
         </div>
       </div>
     </motion.div>
+  )
+}
+
+type PaymentOptionCardProps = {
+  option: TCreditCardOption['opcoes'][number]
+  updatePaymentOption: (data: TCreditCardOption['opcoes'][number]) => void
+  removePaymentOption: () => void
+}
+function PaymentOptionCard({ option, updatePaymentOption, removePaymentOption }: PaymentOptionCardProps) {
+  const [paymentOptionInfo, setPaymentOptionInfo] = useState<TCreditCardOption['opcoes'][number]>(option)
+  const [editPaymentOptionMenuIsOpen, setEditPaymentOptionMenuIsOpen] = useState<boolean>(false)
+  return (
+    <AnimatePresence>
+      <div className="flex w-full flex-col gap-2 rounded border border-gray-500 p-3">
+        <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
+          <div className="flex items-center gap-1">
+            <div className="flex h-[30px] w-[30px] items-center justify-center rounded-full border border-black p-1">
+              <MdPayment />
+            </div>
+            <p className="text-[0.6rem] font-medium leading-none tracking-tight lg:text-xs">{option.descricao}</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <button
+              onClick={() => setEditPaymentOptionMenuIsOpen(true)}
+              className="flex items-center gap-1 rounded-lg bg-blue-600 px-2 py-1 text-[0.6rem] text-white hover:bg-blue-500"
+            >
+              <Pencil width={10} height={10} />
+              <p>EDITAR</p>
+            </button>
+            <button
+              onClick={() => removePaymentOption()}
+              className="flex items-center gap-1 rounded-lg bg-red-600 px-2 py-1 text-[0.6rem] text-white hover:bg-red-500"
+            >
+              <MdDelete width={10} height={10} />
+              <p>REMOVER</p>
+            </button>
+          </div>
+        </div>
+        <h1 className="text-[0.6rem] text-gray-500">TAXAS POR PARCELA</h1>
+        <div className="flex w-full flex-wrap items-center gap-2">
+          {Object.entries(option.parcelas).map(([key, value]) => (
+            <div className="flex items-center gap-1 rounded-lg bg-gray-500 px-2 py-0.5 text-[0.6rem] text-white">
+              <p>{key}x</p>
+              <h1 className="font-black">{formatDecimalPlaces(value)}%</h1>
+            </div>
+          ))}
+        </div>
+        {editPaymentOptionMenuIsOpen ? (
+          <motion.div
+            variants={GeneralVisibleHiddenExitMotionVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="flex w-[90%] flex-col gap-1 self-center overflow-hidden rounded border border-blue-500"
+          >
+            <h1 className="w-full bg-blue-500 p-1 text-center text-xs font-bold text-white lg:text-base">EDITAR OPÇÃO PAGAMENTO</h1>
+            <div className="flex w-full flex-col gap-2 p-3">
+              <TextInput
+                value={paymentOptionInfo.descricao}
+                label="DESCRIÇÃO/BANDEIRA"
+                placeholder="Preencha aqui a descrição/bandeira da opção..."
+                handleChange={(value) => setPaymentOptionInfo((prev) => ({ ...prev, descricao: value }))}
+                width="100%"
+              />
+              <div className="grid w-full grid-cols-1 items-center gap-2 lg:grid-cols-2">
+                {Object.entries(paymentOptionInfo.parcelas).map(([key, value]) => (
+                  <div className={`flex  w-full flex-col gap-1`}>
+                    <label htmlFor={`parcela-${key}`} className={cn('text-xs tracking-tight')}>
+                      TAXA P/ {key} PARCELA(S)
+                    </label>
+                    <input
+                      value={!isEmpty(value) ? value?.toString() : ''}
+                      onChange={(e) => {
+                        setPaymentOptionInfo((prev) => ({
+                          ...prev,
+                          parcelas: {
+                            ...prev.parcelas,
+                            [key]: Number(e.target.value),
+                          },
+                        }))
+                      }}
+                      id={`parcela-${key}`}
+                      type="number"
+                      placeholder={`Preencha aqui a taxa para ${key} parcela(s)`}
+                      className="h-[30px] w-full rounded-md border border-gray-200 p-3 text-xs outline-none placeholder:italic"
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="flex w-full flex-col items-center justify-center gap-2 lg:flex-row lg:justify-end">
+                <Button variant={'outline'} onClick={() => setEditPaymentOptionMenuIsOpen(false)} size={'xs'}>
+                  FECHAR MENU
+                </Button>
+                <Button
+                  onClick={() => {
+                    updatePaymentOption(paymentOptionInfo)
+                    setEditPaymentOptionMenuIsOpen(false)
+                  }}
+                  size={'xs'}
+                  className="bg-blue-600 hover:bg-blue-500"
+                >
+                  ATUALIZAR OPÇÃO
+                </Button>
+              </div>
+            </div>
+          </motion.div>
+        ) : null}
+      </div>
+    </AnimatePresence>
   )
 }

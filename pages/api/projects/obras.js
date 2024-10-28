5cbcf3dfa7667852163d8867b0432eccd95a289f
decctor <lucasfernandes1101@hotmail.com>
2024-10-28 11:17:20 -0300

    fix to obras query filter

--- pages/api/projects/obras.js
+++ pages/api/projects/obras.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
             $or: [
               { $and: [{ tipoDeServico: { $ne: 'MONTAGEM E DESMONTAGEM' } }, { 'compra.dataPedido': { $ne: null } }] },
               { 'obra.statusSolicitacao': 'SOLICITADA' },
+              { 'compra.statusEntrega': 'ENTREGUE' },
             ],
             tipoDeServico: { $nin: ['OPERAÇÃO E MANUTENÇÃO'] },
           },

49996be15173dad2ed9e565f84e60a58f0369812
decctor <lucasfernandes1101@hotmail.com>
2024-11-07 15:31:17 -0300

    fix to projects in supply state query filter

--- pages/api/projects/suprimentos.js
+++ pages/api/projects/suprimentos.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
           $match: {
             tipoDeServico: { $ne: 'OPERAÇÃO E MANUTENÇÃO' },
             'compra.liberacao': true,
-            'compra.status': { $ne: 'CONCLUIDA' },
+            'compra.status': { $ne: 'CONCLUÍDA' },
           },
         },
         {

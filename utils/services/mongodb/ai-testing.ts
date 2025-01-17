f31798092e8b784068ef7066506d65836daeb581
decctor <lucasfernandes1101@hotmail.com>
2025-01-17 14:33:49 -0300

    disable testing ai route

---
+++ utils/services/mongodb/ai-testing.ts
@@ -0,0 +1,14 @@
+import createHttpError from 'http-errors'
+import { Db, MongoClient } from 'mongodb'
+import clientPromise from './mongo-client'
+
+let cachedDb: Db | null = null
+export default async function connectToAITestingDatabase() {
+  if (cachedDb) {
+    return cachedDb
+  }
+  const client = await clientPromise
+  const db = client.db('testando-ia')
+  cachedDb = db
+  return db
+}

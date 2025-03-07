1a7d18034edc1037777f78b9b929626e3e688da6
Decctor <lucasfernandes1101@hotmail.com>
2025-03-07 14:19:21 -0300

    remove import for deleted file causing error

--- pages/api/exportManual.ts
+++ pages/api/exportManual.ts
@@ -13,33 +13,8 @@ import { Collection, type Db, ObjectId } from "mongodb";
 import type { NextApiHandler } from "next";
 import { getContractValue } from "../../utils/methods/util/projects";
 import type { TUser } from "@/utils/schemas/crm/user.schema";
-import ReferralCodes from "../../sellers-referral-codes.json";
 const getExport: NextApiHandler<any> = async (req, res) => {
-	const appDb: Db = await connectToProjectsDatabase();
-	const crmDb: Db = await connectToCRMDatabase();
-
-	const usersCollection = await crmDb.collection<TUser>("users");
-	const projectsCollection = appDb.collection<TProject>("dados");
-
-	const users = await usersCollection.find({}).toArray();
-
-	const usersWithReferalCodes = users.map((user) => {
-		const referralCode = ReferralCodes.find((code) => code._id.$oid === user._id.toString());
-		return {
-			updateOne: {
-				filter: { _id: new ObjectId(user._id) },
-				update: {
-					$set: {
-						codigoIndicacaoConecta: referralCode?.code || "",
-					},
-				},
-			},
-		};
-	});
-
-	const bulkwriteResponse = await usersCollection.bulkWrite(usersWithReferalCodes);
-
-	return res.json(bulkwriteResponse);
+	return res.json("DESATIVADA");
 };
 export default apiHandler({
 	GET: getExport,

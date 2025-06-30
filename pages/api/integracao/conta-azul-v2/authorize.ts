dc44f08b809ba1ab01d58aa462bae8122c48ae02
AmpereProcessos <ampereprocessos@gmail.com>
2025-06-30 14:28:25 -0300

    add new callback for conta azul v2

--- pages/api/integracao/conta-azul-v2/authorize.ts
+++ pages/api/integracao/conta-azul-v2/authorize.ts
@@ -2,10 +2,11 @@ import { apiHandler } from "@/utils/api";
 import type { NextApiHandler } from "next";
 
 const handleContaAzulAuthorize: NextApiHandler<any> = async (req, res) => {
+	console.log("[INFO] [CONTAAZULV2] Running Authorization Flow");
 	const redirect_uri = "https://app.ampereenergias.com.br/api/integracao/conta-azul-v2/callback";
 
-	const url = `https://api.contaazul.com/auth/authorize?redirect_uri=${redirect_uri}&client_id=${process.env.CONTAAZULV2_CLIENT_ID}&scope=Sale&state=12345678`;
-
+	const url = `https://auth.contaazul.com/oauth2/authorize?response_type=code&client_id=${process.env.CONTAAZULV2_CLIENT_ID}&redirect_uri=${redirect_uri}&state=12345678&scope=openid+profile+aws.cognito.signin.user.admin`;
+	console.log("[INFO] [CONTAAZULV2] Redirecting URL: ", url);
 	return res.redirect(url);
 };
 
--- pages/api/integracao/conta-azul-v2/callback.ts
+++ pages/api/integracao/conta-azul-v2/callback.ts
@@ -1,23 +1,38 @@
 import { apiHandler } from "@/utils/api";
 import axios from "axios";
 import type { NextApiHandler } from "next";
+import QueryString from "qs";
 
 const handleContaAzulAuthCallback: NextApiHandler<any> = async (req, res) => {
-	console.log("CONTA AZUL AUTH CALLBACK", req.query);
+	console.log("[INFO] [CONTAAZULV2] [CALLBACK] Received request:", req.query);
 	const { code, state } = req.query;
 
 	if (!code || !state) return res.status(400).json({ error: "Parâmetros inválidos." });
 
-	const redirect_uri = "https://app.ampereenergias.com.br/api/integracao/conta-azul-v2/callback";
-	const headers = {
-		Authorization: `Basic ${Buffer.from(`${process.env.CONTAAZULV2_CLIENT_ID}:${process.env.CONTAAZULV2_CLIENT_SECRET}`).toString("base64")}`,
-	};
+	// Monta o header Authorization Basic com base64 do clientId:clientSecret
+	const basicAuth = Buffer.from(`${process.env.CONTAAZULV2_CLIENT_ID}:${process.env.CONTAAZULV2_CLIENT_SECRET}`).toString("base64");
+
+	const data = QueryString.stringify({
+		client_id: process.env.CONTAAZULV2_CLIENT_ID,
+		client_secret: process.env.CONTAAZULV2_CLIENT_SECRET,
+		grant_type: "authorization_code",
+		code: code,
+		redirect_uri: "https://app.ampereenergias.com.br/api/integracao/conta-azul-v2/callback",
+	});
+
+	console.log("[INFO] [CONTAAZULV2] [CALLBACK] Stringified auth data:", data);
+	console.log("[INFO] [CONTAAZULV2] [CALLBACK] Basic auth:", basicAuth);
 	try {
-		const response = await axios.post(`https://api.contaazul.com/oauth2/token?grant_type=authorization_code&redirect_uri=${redirect_uri}&code=${code}`, {}, { headers });
-		console.log("AUTHTOKEN RESPONSE", response.data);
+		const response = await axios.post("https://auth.contaazul.com/oauth2/token", data, {
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization: `Basic ${basicAuth}`,
+			},
+		});
+		console.log("[INFO] [CONTAAZULV2] Token response:", response.data);
 	} catch (error) {
-		console.log("AUTHTOKEN ERROR", error);
-		throw error;
+		console.log("[ERROR] [CONTAAZULV2] Error fetching token:", error);
+		return res.status(500).json({ error: "Erro ao obter token de acesso." });
 	}
 
 	return res.status(200).json({ message: "Autenticação realizada com sucesso !" });

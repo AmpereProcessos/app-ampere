d29e6d97e31648f6b67688a6dda07f6937bdbf1b
AmpereProcessos <123637067+AmpereProcessos@users.noreply.github.com>
2025-12-10 16:16:11 -0300

    Add DELETE method to expenses API handler

--- pages/api/despesas/index.ts
+++ pages/api/despesas/index.ts
@@ -159,6 +159,7 @@ export default apiHandler({
 	GET: getExpenses,
 	POST: createExpense,
 	PUT: editExpense,
+	DELETE: deleteExpense
 });
 // export default async function handler(req, res) {
 //   if (req.method == 'GET') {

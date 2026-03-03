1040e1206b2424a97693d4679aadc38408acef83
Lucas Fernandes <lucasfernandes1101@hotmail.com>
2026-03-03 16:14:05 -0300

    feat: implement editing and removal functionality for commissioned users in InfoContratoBlock

--- components/blocosInfoProjeto/InfoContratoBlock.tsx
+++ components/blocosInfoProjeto/InfoContratoBlock.tsx
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import React, { type Dispatch, type SetStateAction } from "react";
+import React, { useState, type Dispatch, type SetStateAction } from "react";
 
 import { cn } from "@/lib/utils";
 import { formatDate, formatDecimalPlaces, formatToMoney } from "@/utils/constants";
@@ -7,7 +7,7 @@ import { formatNameAsInitials } from "@/utils/methods/formatting";
 import { formatDateInputChange } from "@/utils/methods/shared";
 import type { TProjectUpdateLogDTO } from "@/utils/schemas/project-updates-logs";
 import type { TProjectComissionedUser, TProjectDTO } from "@/utils/schemas/projects";
-import { BadgeCheck, BadgeDollarSign, Percent } from "lucide-react";
+import { BadgeCheck, BadgeDollarSign, Pencil, Percent } from "lucide-react";
 import { FileSignature } from "lucide-react";
 import { FaPercent } from "react-icons/fa";
 import { contractStatus } from "../../utils/select-options";
@@ -19,6 +19,8 @@ import DateInput from "../inputs/Date";
 import NumberInput from "../inputs/Number";
 import SelectInput from "../inputs/Select";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import ResponsiveDialogDrawer from "../utils/ResponsiveDialogDrawer";
 import { InfoBlockSection } from "./Utils/SectionBlock";
 
 type InfoContratoBlockProps = {
@@ -32,6 +34,28 @@ type InfoContratoBlockProps = {
 	showPaymentInfo?: boolean;
 };
 function InfoContratoBlock({ editor, infoHolder, setInfo, changes, setChanges, updateLogs = [], showPaymentInfo = false }: InfoContratoBlockProps) {
+	function updateComissionedUser({ index, changes }: { index: number; changes: Partial<TProjectComissionedUser> }) {
+		setInfo((prev) => ({
+			...prev,
+			comissoes: {
+				...(prev.comissoes || {}),
+				comissionados: prev.comissoes?.comissionados?.map((comissionedUser, i) => (i === index ? { ...comissionedUser, ...changes } : comissionedUser)),
+			},
+		}));
+		setChanges((prev) => ({
+			...prev,
+			"comissoes.comissionados": infoHolder.comissoes?.comissionados?.map((comissionedUser, i) =>
+				i === index ? { ...comissionedUser, ...changes } : comissionedUser,
+			),
+		}));
+	}
+	function removeComissionedUser(index: number) {
+		setInfo((prev) => ({
+			...prev,
+			comissoes: { ...(prev.comissoes || {}), comissionados: prev.comissoes?.comissionados?.filter((_, i) => i !== index) },
+		}));
+		setChanges((prev) => ({ ...prev, "comissoes.comissionados": infoHolder.comissoes?.comissionados?.filter((_, i) => i !== index) }));
+	}
 	return (
 		<div className="flex flex-col rounded-md border border-primary pb-2 shadow-lg gap-6">
 			<div className="flex items-center gap-2 bg-primary/20 px-2 py-2 rounded w-full justify-center">
@@ -214,11 +238,13 @@ function InfoContratoBlock({ editor, infoHolder, setInfo, changes, setChanges, u
 						<div className="flex w-full flex-col gap-2">
 							<p className="text-sm leading-none font-medium tracking-tight">COMISSIONADOS</p>
 							{infoHolder.comissoes?.comissionados && infoHolder.comissoes?.comissionados.length > 0 ? (
-								infoHolder.comissoes?.comissionados.map((comissionedUser) => (
+								infoHolder.comissoes?.comissionados.map((comissionedUser, index) => (
 									<ComissionableUsersCard
-										key={comissionedUser.idCrm}
+										key={`${comissionedUser.idCrm}-${index}`}
 										comissionedUser={comissionedUser}
 										comissionableValue={infoHolder.comissoes?.valorComissionavel || 0}
+										updateComissionedUser={(changes) => updateComissionedUser({ index, changes })}
+										removeComissionedUser={() => removeComissionedUser(index)}
 									/>
 								))
 							) : (
@@ -254,8 +280,11 @@ export default InfoContratoBlock;
 type ComissionableUsersCardProps = {
 	comissionedUser: TProjectComissionedUser;
 	comissionableValue: number;
+	updateComissionedUser: (changes: Partial<TProjectComissionedUser>) => void;
+	removeComissionedUser: () => void;
 };
-function ComissionableUsersCard({ comissionedUser, comissionableValue }: ComissionableUsersCardProps) {
+function ComissionableUsersCard({ comissionedUser, comissionableValue, updateComissionedUser, removeComissionedUser }: ComissionableUsersCardProps) {
+	const [editMenuIsOpen, setEditMenuIsOpen] = useState(false);
 	return (
 		<div className="border-primary bg-background flex w-full flex-col gap-1 rounded border p-2 shadow-xs dark:bg-[#121212]">
 			<div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row">
@@ -265,6 +294,10 @@ function ComissionableUsersCard({ comissionedUser, comissionableValue }: Comissi
 						<AvatarFallback>{formatNameAsInitials(comissionedUser.nome || "NA")}</AvatarFallback>
 					</Avatar>
 					<p className="text-sm leading-none font-bold tracking-tight">{comissionedUser.nome}</p>
+					<Button size="fit" variant="ghost" className="flex items-center gap-1.5 px-2 py-1 text-xs" onClick={() => setEditMenuIsOpen(true)}>
+						<Pencil className="h-4 w-4 min-h-4 min-w-4" />
+						<p>EDITAR</p>
+					</Button>
 				</div>
 				<div className="flex grow flex-wrap items-center justify-start gap-2">
 					<div className="bg-secondary text-xxs text-primary/80 flex items-center gap-1 rounded-lg px-2 py-0.5 text-center font-bold italic">
@@ -306,6 +339,122 @@ function ComissionableUsersCard({ comissionedUser, comissionableValue }: Comissi
 					</div>
 				</div>
 			</div>
+			{editMenuIsOpen ? (
+				<EditComissionedUserMenu
+					comissionedUser={comissionedUser}
+					comissionableValue={comissionableValue}
+					updateComissionedUser={(changes) => {
+						updateComissionedUser(changes);
+						setEditMenuIsOpen(false);
+					}}
+					removeComissionedUser={removeComissionedUser}
+					closeMenu={() => setEditMenuIsOpen(false)}
+				/>
+			) : null}
 		</div>
+	);
+}
+
+type EditComissionedUserMenuProps = {
+	comissionedUser: TProjectComissionedUser;
+	comissionableValue: number;
+	updateComissionedUser: (changes: Partial<TProjectComissionedUser>) => void;
+	removeComissionedUser: () => void;
+	closeMenu: () => void;
+};
+function EditComissionedUserMenu({
+	comissionedUser,
+	comissionableValue,
+	updateComissionedUser,
+	removeComissionedUser,
+	closeMenu,
+}: EditComissionedUserMenuProps) {
+	const [stateHolder, setStateHolder] = useState<TProjectComissionedUser>(comissionedUser);
+
+	function updateStateHolder(changes: Partial<TProjectComissionedUser>) {
+		setStateHolder((prev) => ({ ...prev, ...changes }));
+	}
+	return (
+		<ResponsiveDialogDrawer
+			menuTitle="EDITAR COMISSIONADO"
+			menuDescription="Edite as informações do comissionado."
+			menuActionButtonText="ATUALIZAR COMISSIONADO"
+			menuCancelButtonText="CANCELAR"
+			actionFunction={() => updateComissionedUser(stateHolder)}
+			actionIsPending={false}
+			stateIsLoading={false}
+			closeMenu={closeMenu}
+		>
+			<div className="flex w-full items-center justify-between">
+				<div className="flex items-center justify-center gap-2">
+					<h1 className="text-[0.6rem] font-bold tracking-tighter">COMISSÃO DO VENDEDOR:</h1>
+					<Avatar className="h-5 w-5">
+						<AvatarImage src={stateHolder.avatar_url || undefined} alt={stateHolder.nome} />
+						<AvatarFallback>{formatNameAsInitials(stateHolder.nome || "")}</AvatarFallback>
+					</Avatar>
+					<h1 className="text-[0.6rem] font-bold tracking-tighter">{stateHolder.nome}</h1>
+				</div>
+				<div
+					className={cn("bg-secondary text-xxs text-primary/80 flex items-center gap-1 rounded-lg px-2 py-0.5 text-center font-bold italic", {
+						"bg-orange-100 text-orange-700": !stateHolder.dataValidacao,
+						"bg-green-100 text-green-700": stateHolder.dataValidacao,
+					})}
+				>
+					<BadgeCheck className={cn("h-3 min-h-3 w-3 min-w-3")} />
+					<p className={cn("text-[0.57rem] font-medium")}>{stateHolder.dataValidacao ? "VALIDADO" : "NÃO VALIDADO"}</p>
+				</div>
+			</div>
+
+			<NumberInput
+				label="COMISSÃO DO VENDEDOR (%)"
+				value={stateHolder.porcentagem}
+				handleChange={(v) => {
+					updateStateHolder({ porcentagem: v, valor: (v / 100) * comissionableValue });
+				}}
+				placeholder="Preencha aqui a porcentagem da comissão do vendedor..."
+				width="100%"
+				labelClassName="text-[0.6rem]"
+				holderClassName="text-xs p-2 min-h-[34px]"
+			/>
+			<NumberInput
+				label="COMISSÃO DO VENDEDOR (R$)"
+				value={stateHolder.valor || null}
+				handleChange={(v) => {
+					updateStateHolder({ valor: v, porcentagem: (v / comissionableValue) * 100 });
+				}}
+				placeholder="Preencha aqui o valor da comissão do vendedor..."
+				width="100%"
+				labelClassName="text-[0.6rem]"
+				holderClassName="text-xs p-2 min-h-[34px]"
+			/>
+			<div className="flex w-full items-center justify-center">
+				<div className="bg-secondary text-primary/80 flex items-center gap-1 rounded-lg px-2 py-0.5 text-center text-[0.55rem] font-bold italic">
+					<p>COMISSÃO FINAL DO VENDEDOR</p>
+					<p className="text-[0.57rem] font-black text-[#15599a]">{formatToMoney(comissionableValue * (stateHolder.porcentagem / 100))}</p>
+				</div>
+			</div>
+			<div className="flex w-full flex-wrap items-center justify-center gap-2">
+				<div className="w-fit">
+					<CheckboxWithDate
+						labelFalse="VALORES NÃO EFETIVADOS"
+						labelTrue="VALORES EFETIVADOS"
+						date={stateHolder.dataEfetivacao ? new Date(stateHolder.dataEfetivacao) : null}
+						handleChange={(value) => {
+							updateStateHolder({ dataEfetivacao: value });
+						}}
+					/>
+				</div>
+				<div className="w-fit">
+					<CheckboxWithDate
+						labelFalse="PAGAMENTO NÃO REALIZADO"
+						labelTrue="PAGAMENTO REALIZADO"
+						date={stateHolder.dataPagamento ? new Date(stateHolder.dataPagamento) : null}
+						handleChange={(value) => {
+							updateStateHolder({ dataPagamento: value });
+						}}
+					/>
+				</div>
+			</div>
+		</ResponsiveDialogDrawer>
 	);
 }
